598cb76118f08043b27e8c8a83053beba2503edf
Jegor van Opdorp <jegorvanopdorp@gmail.com>
2018-02-24 07:40:23 +0100

    unshared bin directory

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + '/src/client'));
-app.use(express.static(__dirname + '/bin'));
 
 app.listen(process.env.PORT || 8080);
 console.log("open http://localhost:8080/ in your favorite broswer ;-)");

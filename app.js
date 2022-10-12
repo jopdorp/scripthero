926ff2a4e4a8e7f1755afed3911baa39bd428964
Jegor van Opdorp <jegor@leaf.cloud>
2022-10-12 18:15:15 +0200

    updated api key and fixed deps

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var express = require('express');
+const cors = require('cors');
 var app = express();
 
+
+const corsOptions = {
+    origin: 'http://localhost:8080'
+};
+
+app.use(cors());
 app.use(express.static(__dirname + '/src/client'));
 
 app.listen(process.env.PORT || 8080);
